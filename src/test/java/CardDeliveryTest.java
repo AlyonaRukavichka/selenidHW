d36b35e8d04fc1e1a98ed1962662d746362c270d
AlyonaRukavichkaa <a.rukavichka14@gmail.com>
2023-08-19 00:09:31 +0300

    first commit

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+class CardDeliveryTest {
+    @Test
+    void shouldRegisterDateOfDeliveryCard() {
+        open("http:// 0.0.0.0:9999");
+    }
+}
