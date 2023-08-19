588484dc6f8d2bd1dea7d191bd1f49ba7fe8921d
AlyonaRukavichkaa <a.rukavichka14@gmail.com>
2023-08-19 16:10:18 +0300

    data

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,10 +1,14 @@
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Selenide.open;
 
 class CardDeliveryTest {
     @Test
     void shouldRegisterDateOfDeliveryCard() {
-        open("http:// localhost:9999");
+        open("http://localhost:9999");
+     //   String possibleDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 }
